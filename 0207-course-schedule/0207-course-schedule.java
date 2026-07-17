ea5688098f653337901a4db40d98c9fff1ff9d63
sanskar8840 <sanskaryadav578@gmail.com>
2026-07-17 10:49:24 +0530

    Time: 8 ms (25.87%), Space: 47.1 MB (49.61%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            List<Integer> l = new ArrayList<>();
+            list.add(l);
+        }
+        // makr in deagree array
+        int[] indegree = new int[numCourses];
+        for (int i = 0; i < prerequisites.length; i++) {
+            // int a=prerequisites[i][1];  // a->b
+            // int b = prerequisites[i][0];
+            // list.get(b).add(a);
+            // indegree[b]++;
+            int a = prerequisites[i][0];
+            int b = prerequisites[i][1];
+
+            list.get(b).add(a);
+            indegree[a]++;
+        }
+
+        int count = 0;
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                q.add(i);
+                count++;
+            }
+
+        }
+        while (q.size() > 0) {
+            int front = q.remove();
+            for (int ele : list.get(front)) {
+                indegree[ele]--;
+                if (indegree[ele] == 0) {
+                    q.add(ele);
+                    count++;
+                }
+
+            }
+        }
+        System.out.println(count);
+        if (count == numCourses)
+            return true;
+        return false;
+    }
+}
