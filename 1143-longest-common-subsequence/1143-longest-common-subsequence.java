0f5953ed98f8f2c643521957e7f2854ccd1b60c0
sanskar8840 <sanskaryadav578@gmail.com>
2026-08-08 13:23:28 +0530

    Time: 33 ms (7.59%), Space: 54.1 MB (20.64%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,25 +1,24 @@
 class Solution {
-    public int helper(int n,int m,StringBuilder a,StringBuilder b,int[][] dp){
-        if(n<0||m<0) return 0;
+    public int helper(int n,int m,StringBuilder t1,StringBuilder t2,int[][] dp){
+        if(n<0 ||m<0) return 0;
         if(dp[n][m]!=-1) return dp[n][m];
-        if(a.charAt(n)==b.charAt(m)){
-            return dp[n][m]= 1+helper(n-1,m-1,a,b,dp);
-        }
-        else{
-            return dp[n][m]= Math.max(helper(n-1,m,a,b,dp),helper(n,m-1,a,b,dp));
+        if(t1.charAt(n)==t2.charAt(m)){
+            return dp[n][m]= 1+helper(n-1,m-1,t1,t2,dp);
+        }else{
+            return dp[n][m]= Math.max(helper(n-1,m,t1,t2,dp),helper(n,m-1,t1,t2,dp));
         }
     }
     public int longestCommonSubsequence(String text1, String text2) {
-        StringBuilder a=new StringBuilder(text1);
-        StringBuilder b=new StringBuilder(text2);
-        int n=a.length();
-        int m=b.length();
-        int[][] dp=new int[n][m];
+        StringBuilder t1=new StringBuilder(text1);
+        StringBuilder t2=new StringBuilder(text2);
+        int n=text1.length();
+        int m=text2.length();
+        int [][] dp= new int[n][m];
         for(int i=0;i<dp.length;i++){
             for(int j=0;j<dp[0].length;j++){
                 dp[i][j]=-1;
             }
         }
-        return helper(n-1,m-1,a,b,dp);
+        return helper(n-1,m-1,t1,t2,dp);
     }
 }
