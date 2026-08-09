a25452e50d9501ad07aba6f09c887e359ffb256e
sanskar8840 <sanskaryadav578@gmail.com>
2026-08-09 23:58:38 +0530

    Time: 11 ms (55.76%), Space: 49.1 MB (50.7%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,53 +1,58 @@
 class Solution {
-    public class pair implements Comparable<pair>{
+    public class pair implements Comparable<pair> {
         int node;
-        int time;
-        pair(int node,int time){
-            this.node=node;
-            this.time=time;
+        int weight;
+
+        pair(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
         }
-        public int compareTo(pair p){
-            if(this.time==p.time) return this.node-p.node;
-            return Integer.compare(this.time,p.time);
+
+        public int compareTo(pair p) {
+            return Integer.compare(this.weight, p.weight);
+
         }
     }
+
     public int networkDelayTime(int[][] times, int n, int k) {
-
-
-        List<List<pair>> list = new ArrayList<>();
-        for(int i=0;i<=n;i++){
-            list.add(new ArrayList<pair>());
+        ArrayList<ArrayList<pair>> adj = new ArrayList<>();
+        
+        for (int i = 0; i <= n; i++) {
+            ArrayList<pair> list = new ArrayList<>();
+            adj.add(list);
         }
-        for(int i=0;i<times.length;i++){
-            int u=times[i][0];
-            int v=times[i][1];
-            int w=times[i][2];
-            list.get(u).add(new pair(v,w));
+        PriorityQueue<pair> pq = new PriorityQueue<>();
+        int[] ans = new int[n + 1];
+        Arrays.fill(ans, Integer.MAX_VALUE);
+        ans[k] = 0;
+        for (int i = 0; i < times.length; i++) {
+            int u = times[i][0];
+            int v = times[i][1];
+            int w = times[i][2];
+            adj.get(u).add(new pair(v, w));
         }
-        int[] ans = new int[n+1];
-        Arrays.fill(ans,Integer.MAX_VALUE);
-        ans[k]=0;
-        PriorityQueue<pair> pq=new PriorityQueue<>();
-        pq.add(new pair (k,0));
-        while(pq.size()>0){
-            pair top =pq.remove();
-            int node =top.node;
-            int time=top.time;
-            for(pair ele : list.get(node)){
-                int totaltime=top.time+ele.time;
-                if(totaltime<ans[ele.node]){
-                    ans[ele.node]=totaltime;
-                    pq.add(new pair (ele.node,totaltime));
+        pq.add(new pair(k, 0));
+        while (pq.size() > 0) {
+            pair front = pq.remove();
+            int node1 = front.node;
+            int w1 = front.weight;
+            for (pair ele : adj.get(node1)) {
+                int node2 = ele.node;
+                int w2 = ele.weight;
+                if (w1 + w2 < ans[node2]) {
+                    ans[node2] = w1 + w2;
+                    pq.add(new pair(node2, w1 + w2));
                 }
             }
+
         }
-        int max=-1;
-        for(int i=1;i<ans.length;i++){
-            if(ans[i]==Integer.MAX_VALUE) return -1;
-            max =Math.max(ans[i],max);
+        int min = 0;
+        for (int i = 1; i < ans.length; i++) {
+            if (ans[i] == Integer.MAX_VALUE)
+                return -1;
+            min = Math.max(min, ans[i]);
         }
-        return max;
-        
-        
+        return min;
+
     }
 }
