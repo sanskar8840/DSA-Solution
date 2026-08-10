db5f2310782115d830503625e1de566c74339496
sanskar8840 <sanskaryadav578@gmail.com>
2026-08-11 01:39:29 +0530

    Time: 2 ms (99.34%), Space: 47.1 MB (40.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
+        HashMap <Integer,Integer> mp=new HashMap <>();
+        int[] m= {-1};
+        for(int i=0;i<nums.length;i++){
+            int found =target -nums[i];
+            if(mp.containsKey(found)){
+              int[] ans={i,mp.get(found)};
+                return ans;
+            }
+            mp.put(nums[i],i);
+        }
+        return m;
     }
 }
